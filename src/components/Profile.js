e7f722df532d4af5a5a2a2969442c8fddfc11a01
Bin Gao <gaobin1019@gmail.com>
2018-01-16 16:41:42 -0800

    put player img and team logo to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX } from "../constants";
 
 export class Profile extends React.Component {
     render() {
+        const {
+            teamAbbreviation,
+            teamCity,
+            teamName,
+            playerName,
+            height,
+            weight,
+            pts, reb, ast, pie,
+        } = this.props.playerInfo;
+
         return (
             <div className='profile'>
-                
+                <img className='playerLogo' src={`${PROFILE_PIC_URL_PREFIX}/${this.props.playerId}.png`} alt="playerImg" />
+                <img className='teamLogo' src={`${TEAM_PIC_URL_PREFIX}/${teamAbbreviation}_logo.svg`} alt='teamImg'/>
             </div>
         );
     }
